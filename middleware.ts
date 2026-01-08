5655221e06a419543b72095a54f9de477598507f
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2026-01-08 20:15:05 +0545

    Admin Protected Route Implemented

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request:NextRequest) {
+export const middleware = (request:NextRequest) => {
     const token = request.cookies.get('accessToken')?.value;
     const {pathname} = request.nextUrl;
 
     const isAuthPage = pathname.startsWith('/auth');
     const isProtectedPage = pathname.startsWith(`/profile`);
-    if(isProtectedPage && !token) {
+    const isAdminPage = pathname.startsWith('/admin');
+
+    if((isProtectedPage || isAdminPage) && !token) {
         return NextResponse.redirect(new URL('/auth',request.url));
     }
 
     if(isAuthPage && token) {
         return NextResponse.redirect(new URL('/',request.url));
     }
+
+    if(isAdminPage && token) {
+        const payload = JSON.parse(Buffer.from(token.split(".")[1],'base64').toString());
+        if(payload.role !== "ADMIN") {
+            return NextResponse.redirect(new URL("/",request.url));
+        }
+    }
     return NextResponse.next();
 }
+
+export const config = {
+    matcher:['/auth/:path*',"/profile/:path*","/admin/:path*"],
+};
