fc3186a284dd8929c91324fc296f4c6a543163c3
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2026-01-12 17:31:58 +0545

    Fix interceptor bug

--- utils/interceptors/axiosInterceptor.ts
+++ utils/interceptors/axiosInterceptor.ts
@@ -1,27 +1,26 @@
 import axios from "axios";
-// import Cookies from "js-cookie";
 export const axiosInstance = axios.create({
-    baseURL:process.env.NEXT_PUBLIC_URL,
-    withCredentials:true
-})
+  baseURL: process.env.NEXT_PUBLIC_URL,
+  withCredentials: true,
+});
 
-axiosInstance.interceptors.request.use((config) => {
-    // const token = Cookies.get("token");
-    // if(token) {
-    //     config.headers.Authorization=`Bearer ${token}`;
-    // }
-    return config;
-},(error) => Promise.reject(error));
+axiosInstance.interceptors.request.use(
+  (config) => config,
+  (error) => Promise.reject(error)
+);
 
-axiosInstance.interceptors.response.use((response) => response,async (error) => {
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
     const originalRequest = error.config;
-
-    if(error?.response.status === 401 && !originalRequest._retry) {
-        originalRequest._retry=true;
-        await axiosInstance.post('/auth/refresh');
-        return axios(originalRequest);
-        // console.warn("Unauthorized! Redirecting to Auth page");
-        // window.location.href="/auth"
+    console.log(originalRequest)
+    if (error?.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      await axiosInstance.post("/auth/refresh");
+      return axios(originalRequest);
+      // console.warn("Unauthorized! Redirecting to Auth page");
+      // window.location.href="/auth"
     }
     return Promise.reject(error);
-})
+  }
+);
