6b5de5738748abc49f5ec8ab8bea56abfc710d6d
Pranaya Ghimire <pranayaghimire88083@gmail.com>
2026-01-25 23:06:36 +0545

    Foods page ui redesign

--- components/foods/AllFoods.tsx
+++ components/foods/AllFoods.tsx
@@ -1,8 +1,22 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Card } from "@/components/ui/card";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { useGetFoods } from "@/utils/apis/foodApi";
-import { Eye, Search, UtensilsCrossed } from "lucide-react";
+import {
+  CircleCheckBig,
+  CircleX,
+  Eye,
+  Search,
+  UtensilsCrossed,
+} from "lucide-react";
 import Image from "next/image";
 import toast from "react-hot-toast";
 import { Skeleton } from "../ui/skeleton";
@@ -55,13 +69,19 @@ import {
 const AllFoods = () => {
   // States
   const [searchValue, setSearchValue] = useState("");
-  const [debounced,setDebounced] = useState(searchValue);
+  const [debounced, setDebounced] = useState(searchValue);
   const [categoryFilter, setCategoryFilter] = useState("All");
   const [priceFilter, setPriceFilter] = useState("Default");
   const [currentPage, setCurrentPage] = useState(1);
+  const isSearching = searchValue !== debounced;
   const router = useRouter();
   const { data: profile } = useGetProfile();
-  const { data: foods, isLoading } = useGetFoods(currentPage, debounced,categoryFilter,priceFilter);
+  const { data: foods, isLoading } = useGetFoods(
+    currentPage,
+    debounced,
+    categoryFilter,
+    priceFilter,
+  );
   const { mutate: createOrder } = useCreateOrder();
   const queryClient = useQueryClient();
   const onOrder = (id: string, isAvailable: boolean, name: string) => {
@@ -78,9 +98,9 @@ const AllFoods = () => {
     else toast.error(`Sorry ${name} is not available at this moment`);
   };
   useEffect(() => {
-    const timer = setTimeout(() => setDebounced(searchValue),400);
+    const timer = setTimeout(() => setDebounced(searchValue), 400);
     return () => clearTimeout(timer);
-  },[searchValue]);
+  }, [searchValue]);
   return (
     <div className="space-y-4">
       <div className="w-full flex flex-col md:flex-row gap-2 justify-between">
@@ -94,6 +114,7 @@ const AllFoods = () => {
             className="bg-white pl-8"
           />
           <Search className="absolute top-3 left-2 text-cyan-500" size={20} />
+          {isSearching && <p>Searching...</p>}
         </div>
         {/* Filters */}
         <div className="w-fit flex gap-3 space-y-2">
@@ -122,27 +143,83 @@ const AllFoods = () => {
           </NativeSelect>
         </div>
       </div>
-      {/* <div className="flex-wrap">
-        {foods?.data?.map((food:{_id:string;name:string;price:string;image:string;isAvailable:boolean}) => 
-          <Card key={food._id} className="max-w-sm">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5  ">
+        {foods?.data?.map(
+          (food: {
+            _id: string;
+            name: string;
+            price: string;
+            image: string;
+            isAvailable: boolean;
+          }) => (
+            <Card key={food._id} className="shadow-md hover:-translate-y-3 transition-all duration-400 ease-in-out border-t-4 border-t-cyan-500">
               <CardHeader>
-                  <CardTitle>{food.name}</CardTitle>
-                  <CardDescription>Rs. {food.price}</CardDescription>
-                  <CardAction className="">
-                    <Button variant={`primary`}><UtensilsCrossed/> Order</Button>
-                  </CardAction>
+                <CardTitle>{food.name}</CardTitle>
+                <CardDescription className="text-[18px]">Rs. {food.price} only</CardDescription>
+                <CardAction className="">
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button variant={`primary`}>
+                        <UtensilsCrossed />
+                        Order
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          Are you sure you want to order {food.name} ?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          This action cannot be undone. {food.name} will be
+                          ordered from your account.
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel className="cursor-pointer">
+                          Cancel
+                        </AlertDialogCancel>
+                        <AlertDialogAction
+                          onClick={() =>
+                            onOrder(food._id, food.isAvailable, food.name)
+                          }
+                          className="bg-cyan-500 hover:bg-cyan-600 cursor-pointer"
+                        >
+                          Continue
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </CardAction>
               </CardHeader>
               <CardContent className="">
                 <div className=" relative w-60 h-60 rounded-md overflow-hidden">
-                     <Image src={food.image} alt={food.name} fill className="object-cover"/>
+                  <Image
+                    src={food.image}
+                    alt={food.name}
+                    fill
+                    className="object-cover"
+                  />
                 </div>
               </CardContent>
               <CardFooter>
-                  {food.isAvailable ? "Available" :"Not available"}
+                <div
+                  className={`${food.isAvailable ? "bg-green-500" : "bg-red-500"} text-white p-2 rounded-md `}
+                >
+                  {food.isAvailable ? (
+                    <p className="flex items-center gap-1">
+                      <CircleCheckBig size={20} /> Available{" "}
+                    </p>
+                  ) : (
+                    <p className="flex items-center gap-1">
+                      <CircleX size={20} /> Not available
+                    </p>
+                  )}
+                </div>
               </CardFooter>
-          </Card>
+            </Card>
+          ),
         )}
-      </div> */}
+      </div>
       {isLoading ? (
         <Skeleton className="w-full h-60 bg-gray-400" />
       ) : (
@@ -159,88 +236,83 @@ const AllFoods = () => {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {foods?.data
-                ?.map(
-                  (
-                    f: {
-                      _id: string;
-                      name: string;
-                      price: string;
-                      isAvailable: boolean;
-                      image: string;
-                    },
-                    index: number,
-                  ) => (
-                    <TableRow key={f._id}>
-                      <TableCell className="text-center">{index + 1}</TableCell>
-                      <TableCell className="text-center">{f.name}</TableCell>
-                      <TableCell className="text-center">
-                        Rs. {f.price}
-                      </TableCell>
-                      <TableCell className="text-center">
-                        <Switch checked={f.isAvailable} />
-                      </TableCell>
-                      <TableCell className="text-center space-x-3">
-                        <Dialog>
-                          <DialogTrigger asChild>
-                            <Button className="bg-gray-500 border-2 hover:border-gray-500 hover:bg-white hover:text-gray-500 transition-all duration-400 cursor-pointer">
-                              <Eye />
-                              View
-                            </Button>
-                          </DialogTrigger>
-                          <DialogContent className="w-72">
-                            <DialogHeader>
-                              <DialogTitle>{f.name}</DialogTitle>
-                              <DialogDescription>
-                                Rs. {f.price}
-                              </DialogDescription>
-                            </DialogHeader>
-                            <div className="w-60 h-60 relative rounded-md overflow-hidden">
-                              <Image
-                                src={f.image}
-                                alt={f.name}
-                                fill
-                                className="object-cover"
-                              />
-                            </div>
-                          </DialogContent>
-                        </Dialog>{" "}
-                        <AlertDialog>
-                          <AlertDialogTrigger asChild>
-                            <Button variant={`primary`}>
-                              <UtensilsCrossed />
-                              Order
-                            </Button>
-                          </AlertDialogTrigger>
-                          <AlertDialogContent>
-                            <AlertDialogHeader>
-                              <AlertDialogTitle>
-                                Are you sure you want to order {f.name} ?
-                              </AlertDialogTitle>
-                              <AlertDialogDescription>
-                                This action cannot be undone. {f.name} will be
-                                ordered from your account.
-                              </AlertDialogDescription>
-                            </AlertDialogHeader>
-                            <AlertDialogFooter>
-                              <AlertDialogCancel className="cursor-pointer">
-                                Cancel
-                              </AlertDialogCancel>
-                              <AlertDialogAction
-                                onClick={() =>
-                                  onOrder(f._id, f.isAvailable, f.name)
-                                }
-                                className="bg-cyan-500 hover:bg-cyan-600 cursor-pointer"
-                              >
-                                Continue
-                              </AlertDialogAction>
-                            </AlertDialogFooter>
-                          </AlertDialogContent>
-                        </AlertDialog>
-                      </TableCell>
-                    </TableRow>
-                  ),
-                )}
+              {foods?.data?.map(
+                (
+                  f: {
+                    _id: string;
+                    name: string;
+                    price: string;
+                    isAvailable: boolean;
+                    image: string;
+                  },
+                  index: number,
+                ) => (
+                  <TableRow key={f._id}>
+                    <TableCell className="text-center">{index + 1}</TableCell>
+                    <TableCell className="text-center">{f.name}</TableCell>
+                    <TableCell className="text-center">Rs. {f.price}</TableCell>
+                    <TableCell className="text-center">
+                      <Switch checked={f.isAvailable} />
+                    </TableCell>
+                    <TableCell className="text-center space-x-3">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button className="bg-gray-500 border-2 hover:border-gray-500 hover:bg-white hover:text-gray-500 transition-all duration-400 cursor-pointer">
+                            <Eye />
+                            View
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent className="w-72">
+                          <DialogHeader>
+                            <DialogTitle>{f.name}</DialogTitle>
+                            <DialogDescription>Rs. {f.price}</DialogDescription>
+                          </DialogHeader>
+                          <div className="w-60 h-60 relative rounded-md overflow-hidden">
+                            <Image
+                              src={f.image}
+                              alt={f.name}
+                              fill
+                              className="object-cover"
+                            />
+                          </div>
+                        </DialogContent>
+                      </Dialog>{" "}
+                      <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                          <Button variant={`primary`}>
+                            <UtensilsCrossed />
+                            Order
+                          </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>
+                              Are you sure you want to order {f.name} ?
+                            </AlertDialogTitle>
+                            <AlertDialogDescription>
+                              This action cannot be undone. {f.name} will be
+                              ordered from your account.
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel className="cursor-pointer">
+                              Cancel
+                            </AlertDialogCancel>
+                            <AlertDialogAction
+                              onClick={() =>
+                                onOrder(f._id, f.isAvailable, f.name)
+                              }
+                              className="bg-cyan-500 hover:bg-cyan-600 cursor-pointer"
+                            >
+                              Continue
+                            </AlertDialogAction>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    </TableCell>
+                  </TableRow>
+                ),
+              )}
             </TableBody>
           </Table>
           <Pagination>
@@ -249,10 +321,7 @@ const AllFoods = () => {
                 <PaginationPrevious
                   className="cursor-pointer"
                   onClick={() =>
-                    currentPage >1 &&
-                    setCurrentPage(
-                      (prev) => (prev - 1),
-                    )
+                    currentPage > 1 && setCurrentPage((prev) => prev - 1)
                   }
                 />
               </PaginationItem>
@@ -270,8 +339,13 @@ const AllFoods = () => {
                 <PaginationEllipsis />
               </PaginationItem>
               <PaginationItem>
-                <PaginationNext className="cursor-pointer" 
-                onClick={() => currentPage < foods?.totalPages && setCurrentPage(prev => prev+1)} />
+                <PaginationNext
+                  className="cursor-pointer"
+                  onClick={() =>
+                    currentPage < foods?.totalPages &&
+                    setCurrentPage((prev) => prev + 1)
+                  }
+                />
               </PaginationItem>
             </PaginationContent>
           </Pagination>
